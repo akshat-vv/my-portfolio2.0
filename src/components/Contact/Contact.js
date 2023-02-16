1df31c2ac0d993d74956228dd3a819797d66481c
akshat-vv <akshatvijay14@gmail.com>
2023-02-16 18:33:57 +0530

    contact bg change

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -62,18 +62,6 @@ const Contact = ({ setIsLoading }) => {
           <input type="submit" value="Send" className="button" />
         </form>
         {showModal && <Modal setShowModal={setShowModal} />}
-        {/* {isLoading && (
-          <BallTriangle
-            height={100}
-            width={100}
-            radius={5}
-            color="#4fa94d"
-            ariaLabel="ball-triangle-loading"
-            wrapperClass={{}}
-            wrapperStyle=""
-            visible={true}
-          />
-        )} */}
       </div>
     </div>
   );
