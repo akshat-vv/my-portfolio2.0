6567d0e6e217cc795c830f7addb9737be5ca944b
akshat-thg <Akshat.V62@thg.com>
2025-03-15 13:57:50 +0530

    fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <div className="footer-container">
-      Design | Developed by Akshat Vijayvergiya © 2023-2024
+      Design | Developed by Akshat Vijayvergiya © 2025-2026
     </div>
   );
 };
